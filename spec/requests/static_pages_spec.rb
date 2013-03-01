75d884ebdcf986eba5b24b49385a6e47327eb26a
hector <hecad@hotmail.com>
2013-03-01 15:07:40 -0300

    Test links on layout go to the right pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,4 +29,19 @@ describe "Static pages" do
     before { visit contact_path }
     it_should_behave_like "all static pages", {heading: 'Contact', page_title: 'Contact'}
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
+  end
 end
