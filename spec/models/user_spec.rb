bb92b3984a7777a5a1b7c4e3e674462686c8b926
hector <hecad@hotmail.com>
2013-03-10 15:54:06 -0300

    Add admin (not accesible) attribute to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
 #  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
 #
 
 require 'spec_helper'
@@ -25,9 +26,28 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "accessible attributes" do
+    it "should not allow access to admin attribute" do
+      expect do
+        User.new(name: "Admin User", email: "admin_user@example.com",
+                 password: "foobar",   password_confirmation: "foobar", admin: true)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end    
+  end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
