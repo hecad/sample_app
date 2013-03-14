810082852798b193694d504f2939cf2047db3ca0
hector <hecad@hotmail.com>
2013-03-14 17:31:55 -0300

    User respond to followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,12 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
+  
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   before_save { email.downcase! }
   before_save :create_remember_token
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,6 +32,7 @@ describe User do
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
   it { should respond_to(:followed_users) }
+  it { should respond_to(:followers) }
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
   it { should respond_to(:unfollow!) }
@@ -189,8 +190,13 @@ describe User do
     end
   end
 
-  describe "relationship associations" do
-    pending
+  describe "relationship associations destroy" do
+    describe "when deleting a user it should destroy associations with its follower_id" do
+      pending
+    end
+    describe "when deleting a user it should destroy associations with its followed_id" do
+      pending
+    end
   end
 
   describe "following" do
@@ -202,6 +208,11 @@ describe User do
 
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
 
     describe "and unfollowing" do
       before { @user.unfollow!(other_user) }
